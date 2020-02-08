9882b7b42f28c537f30f5c1b4b4a3a99acf84eec
Adarsh <adarshjain9628@gmail.com>
2020-02-08 18:42:06 +0530

    'amother temp commit'

--- src/lib/Dropdown/DDropdown.vue
+++ src/lib/Dropdown/DDropdown.vue
@@ -1,14 +1,16 @@
 <template>
-    <div style="position: relative">
+    <div style="position: relative;display: inline-flex;">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <d-button @click="isVisible = !isVisible">Sample button</d-button>
+        <d-button type="milky" @click="isVisible = !isVisible" icon="arrow_drop_down" icon-position="right">Sample button</d-button>
         <transition name="popup-in">
-            <d-popup :pos="{top: 36, left: 0}" v-if="isVisible" anchor="bottom_left">
+            <d-popup :pos="{top: 36,right:0}" v-if="isVisible" anchor="bottom_right">
                 <div class="shadow-md rounded-6 p-2 bg-white">
                     <d-list
-                            v-for='opt in ["Vilgax", "Four Arms", "Brainstorm", "Grey Matter", "Dragizazor X 10"]'
+                            style="white-space: nowrap;"
+                            v-for='opt in ["Vilgaxskhbdfg", "Four Arms", "Brainstorm", "Grey Matter", "Dragizazor X 10"]'
                             :key="opt"
                             :label="opt"
+                            desc="kfhbhkbgdfk"
                             icon="delete"
                     />
                 </div>
